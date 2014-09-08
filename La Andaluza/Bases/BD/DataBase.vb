fb5836c4a3ed6c4d7d151cea0aced69308e4333d
miguel_vega_menacho <miguel.vega.menacho@gmail.com>
2014-09-08 11:39:23 +0200

    no message

--- La Andaluza/Bases/BD/DataBase.vb
+++ La Andaluza/Bases/BD/DataBase.vb
@@ -46,6 +46,7 @@ Public Class DataBase
         If server = SERVIDOR Then
             'Name = "192.168.1.101\SQLEXPRESS,1608"
             Name = "192.168.1.200"
+            'Name = "SERVIDOR"
             Config.connectionString = "User ID=ssa;Password=Trucha0122;Trusted_Connection=False;"
         Else
 
