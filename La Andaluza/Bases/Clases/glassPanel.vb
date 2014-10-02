a6c57444ed252ec87a0c373d11db698d0b612b06
miguel_vega_menacho <miguel.vega.menacho@gmail.com>
2014-10-02 15:59:00 +0200

    no message

--- La Andaluza/Bases/Clases/glassPanel.vb
+++ La Andaluza/Bases/Clases/glassPanel.vb
@@ -6,12 +6,20 @@
         padre = frm
     End Sub
     Public Sub recolocar(sender As Object, e As EventArgs)
-        Me.Size = New Size(padre.Size.Width + 200, padre.Size.Height + 100)
-        Me.Location = New Point(padre.Location.X - 100, padre.Location.Y - 50)
+        'Me.Size = New Size(padre.Size.Width + 200, padre.Size.Height + 100)
+        'Me.Location = New Point(padre.Location.X - 100, padre.Location.Y - 50)
+        Me.FormBorderStyle = Windows.Forms.FormBorderStyle.FixedSingle
+
+        Me.Location = New Point(padre.Location.X, padre.Location.Y)
+        
         Me.Visible = True
+        Me.WindowState = FormWindowState.Maximized
+        Me.FormBorderStyle = Windows.Forms.FormBorderStyle.None
     End Sub
     Public Sub ocultar(sender As Object, e As EventArgs)
-        Me.Visible = False
+        Me.Visible = False        
+        Me.WindowState = FormWindowState.Normal
+        Me.Size = padre.Size
     End Sub
 
     Private Sub glassPanel_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
