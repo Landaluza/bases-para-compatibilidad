084e37345b2fde726218d188a0a65651948a3f01
miguel_vega_menacho <miguel.vega.menacho@gmail.com>
2014-10-21 11:36:16 +0200

    no message

--- La Andaluza/Bases/BD/BD.vb
+++ La Andaluza/Bases/BD/BD.vb
@@ -62,66 +62,66 @@ Public Module BD
     End Sub
 #End Region
 
-#Region "Consultas Alejandro"
-    Private Linea As String
+    '#Region "Consultas Alejandro"
+    '    Private Linea As String
 
-    Public Function RealizarConsulta(ByVal Cadena As String) As DataTable
-        Dim dtsTabla As New DataTable
-        If transaction Is Nothing Then Conectar()
-        Try
-            dtsTabla = EjecutarRealizarConsulta(Cadena)
-            Return dtsTabla
-        Catch ex As Exception
-            Return Nothing
-        Finally
-            If transaction Is Nothing Then Cerrar()
-        End Try
-    End Function
-
-
-
-    Public Function EjecutarRealizarConsulta(ByVal strRealizarConsulta As String) As DataTable
-        Dim dtsTemp As New DataSet
-        Dim cmd As System.Data.SqlClient.SqlCommand = Nothing
-
-        Try
-            cmd = New System.Data.SqlClient.SqlCommand(strRealizarConsulta, Cnx)
-            If Not transaction Is Nothing Then cmd.Transaction = transaction
-            cmd.CommandTimeout = 300
-            Dim Ad As New System.Data.SqlClient.SqlDataAdapter(cmd)
-            Ad.Fill(dtsTemp, "NuevaTabla")
-            cmd.Dispose()
-        Catch ex As Exception
-            cmd.Dispose()
-            MessageBox.Show("problema en BD.EjecutarRealizarConsulta Ad.Fill" & Environment.NewLine & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-        End Try
-
-        Return dtsTemp.Tables(0)
-    End Function
-
-    Public Function realizarConsultaAlteraciones(ByVal strrealizarConsulta As String) As Integer
-        Dim exito As Integer
-        If transaction Is Nothing Then Conectar()
-        Dim cmd As System.Data.SqlClient.SqlCommand
-        cmd = New System.Data.SqlClient.SqlCommand(strrealizarConsulta, Cnx)
-        If Not transaction Is Nothing Then cmd.Transaction = transaction
-        Try
-            cmd.ExecuteNonQuery()
-            exito = 1
-        Catch ex As Exception
-            MessageBox.Show("Ha ocurrido un error. Detalles:" & Environment.NewLine & Environment.NewLine & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-            exito = 0
-        End Try
-        cmd.Dispose()
-        If transaction Is Nothing Then Cerrar()
-        Return exito
-    End Function
+    '    Public Function RealizarConsulta(ByVal Cadena As String) As DataTable
+    '        Dim dtsTabla As New DataTable
+    '        If transaction Is Nothing Then Conectar()
+    '        Try
+    '            dtsTabla = EjecutarRealizarConsulta(Cadena)
+    '            Return dtsTabla
+    '        Catch ex As Exception
+    '            Return Nothing
+    '        Finally
+    '            If transaction Is Nothing Then Cerrar()
+    '        End Try
+    '    End Function
 
 
-    
+
+    '    Public Function EjecutarRealizarConsulta(ByVal strRealizarConsulta As String) As DataTable
+    '        Dim dtsTemp As New DataSet
+    '        Dim cmd As System.Data.SqlClient.SqlCommand = Nothing
+
+    '        Try
+    '            cmd = New System.Data.SqlClient.SqlCommand(strRealizarConsulta, Cnx)
+    '            If Not transaction Is Nothing Then cmd.Transaction = transaction
+    '            cmd.CommandTimeout = 300
+    '            Dim Ad As New System.Data.SqlClient.SqlDataAdapter(cmd)
+    '            Ad.Fill(dtsTemp, "NuevaTabla")
+    '            cmd.Dispose()
+    '        Catch ex As Exception
+    '            cmd.Dispose()
+    '            MessageBox.Show("problema en BD.EjecutarRealizarConsulta Ad.Fill" & Environment.NewLine & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+    '        End Try
+
+    '        Return dtsTemp.Tables(0)
+    '    End Function
+
+    '    Public Function realizarConsultaAlteraciones(ByVal strrealizarConsulta As String) As Integer
+    '        Dim exito As Integer
+    '        If transaction Is Nothing Then Conectar()
+    '        Dim cmd As System.Data.SqlClient.SqlCommand
+    '        cmd = New System.Data.SqlClient.SqlCommand(strrealizarConsulta, Cnx)
+    '        If Not transaction Is Nothing Then cmd.Transaction = transaction
+    '        Try
+    '            cmd.ExecuteNonQuery()
+    '            exito = 1
+    '        Catch ex As Exception
+    '            MessageBox.Show("Ha ocurrido un error. Detalles:" & Environment.NewLine & Environment.NewLine & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+    '            exito = 0
+    '        End Try
+    '        cmd.Dispose()
+    '        If transaction Is Nothing Then Cerrar()
+    '        Return exito
+    '    End Function
 
 
-#End Region
+
+
+
+    '#End Region
 
 
 
--- La Andaluza/Bases/Interfaces/wizardable.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Interface wizardable
-    Function comprobarCampos() As Boolean
-    Function recuperarValor(ByVal nombre As String) As Object
-    Function grabarDatos() As Boolean
-    Sub EstablecerValores()
-
-End Interface
